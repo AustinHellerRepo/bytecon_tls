efae83cb752c2839ee3a21b701fdb0a5f1620b90
unknown <galaky2005@gmail.com>
2024-11-15 17:57:20 -0500

    refactored certificate logic such that everything is a ByteConverter;

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, future::Future, marker::PhantomData, net::SocketAddr, path::PathBuf, sync::Arc};
+use std::{error::Error, fs::File, future::Future, io::{Read, Seek, SeekFrom, Write}, marker::PhantomData, net::SocketAddr, path::PathBuf, sync::Arc};
 use bytecon::{ByteConverter, ByteStreamReaderAsync, ByteStreamWriterAsync};
 use tokio::net::{TcpListener, TcpStream};
 use tokio_rustls::{rustls::{Certificate, ClientConfig, PrivateKey, RootCertStore, ServerConfig, ServerName}, TlsAcceptor, TlsConnector, TlsStream};
@@ -6,7 +6,7 @@ use tokio_rustls::{rustls::{Certificate, ClientConfig, PrivateKey, RootCertStore
 pub struct ByteConClient<TRequest: ByteConverter, TResponse: ByteConverter> {
     server_address: String,
     server_port: u16,
-    server_public_key_file_path: PathBuf,
+    server_public_key: ByteConPublicKey,
     server_domain: String,
     phantom_request: PhantomData<TRequest>,
     phantom_response: PhantomData<TResponse>,
@@ -14,11 +14,16 @@ pub struct ByteConClient<TRequest: ByteConverter, TResponse: ByteConverter> {
 }
 
 impl<TRequest: ByteConverter, TResponse: ByteConverter> ByteConClient<TRequest, TResponse> {
-    pub fn new(server_address: String, server_port: u16, server_public_key_file_path: PathBuf, server_domain: String) -> Self {
+    pub fn new(
+        server_address: String,
+        server_port: u16,
+        server_public_key: ByteConPublicKey,
+        server_domain: String
+    ) -> Self {
         Self {
             server_address,
             server_port,
-            server_public_key_file_path,
+            server_public_key,
             server_domain,
             phantom_request: PhantomData::default(),
             phantom_response: PhantomData::default(),
@@ -27,13 +32,9 @@ impl<TRequest: ByteConverter, TResponse: ByteConverter> ByteConClient<TRequest, 
     async fn connect(&self) -> Result<TlsStream<TcpStream>, Box<dyn Error>> {
         
         let config = {
-            let cert_file = std::fs::File::open(&self.server_public_key_file_path)?;
-            let mut reader = std::io::BufReader::new(cert_file);
-            let certs = rustls_pemfile::certs(&mut reader)?;
-
             let mut root_cert_store = RootCertStore::empty();
-            for cert in certs {
-                root_cert_store.add(&Certificate(cert))?;
+            for cert in self.server_public_key.get_certificates()? {
+                root_cert_store.add(&cert)?;
             }
 
             ClientConfig::builder()
@@ -68,6 +69,7 @@ impl<TRequest: ByteConverter, TResponse: ByteConverter> ByteConClient<TRequest, 
             } => {
                 Ok(server_response)
             },
+            #[allow(unreachable_patterns)]
             _ => {
                 Err(ServerClientByteConError::UnexpectedServerResponse {
                     server_request: String::from(std::any::type_name::<TRequest>()),
@@ -75,6 +77,187 @@ impl<TRequest: ByteConverter, TResponse: ByteConverter> ByteConClient<TRequest, 
                 }.into())
             }
         }
+    }
+}
+
+impl<TRequest, TResponse> ByteConverter for ByteConClient<TRequest, TResponse>
+where
+    TRequest: ByteConverter,
+    TResponse: ByteConverter,
+{
+    fn append_to_bytes(&self, bytes: &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
+        self.server_address.append_to_bytes(bytes)?;
+        self.server_port.append_to_bytes(bytes)?;
+        self.server_public_key.append_to_bytes(bytes)?;
+        self.server_domain.append_to_bytes(bytes)?;
+
+        Ok(())
+    }
+    fn extract_from_bytes(bytes: &Vec<u8>, index: &mut usize) -> Result<Self, Box<dyn Error>> where Self: Sized {
+        Ok(Self::new(
+            String::extract_from_bytes(bytes, index)?,
+            u16::extract_from_bytes(bytes, index)?,
+            ByteConPublicKey::extract_from_bytes(bytes, index)?,
+            String::extract_from_bytes(bytes, index)?,
+        ))
+    }
+}
+
+pub enum ByteConCertificate {
+    FilePath(PathBuf),
+    RawCertsBytes(Vec<Vec<u8>>),
+}
+
+impl ByteConverter for ByteConCertificate {
+    fn append_to_bytes(&self, bytes: &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
+        match self {
+            Self::FilePath(file_path) => {
+                // byte
+                0u8.append_to_bytes(bytes)?;
+
+                // PathBuf
+                file_path.append_to_bytes(bytes)?;
+
+                // file contents
+                let server_public_key_file_bytes = {
+                    let mut file = File::open(file_path)?;
+                    let position = file.seek(SeekFrom::End(0))? as usize;
+                    let mut file_bytes = Vec::with_capacity(position);
+                    file.seek(SeekFrom::Start(0))?;
+                    let file_size = file.read_to_end(&mut file_bytes)?;
+                    if position != file_size {
+                        return Err(ServerClientByteConError::UnexpectedServerPublicKeyFileSize {
+                            actual_file_size: file_size,
+                            last_position: position,
+                        }.into());
+                    }
+                    file_bytes
+                };
+                server_public_key_file_bytes.append_to_bytes(bytes)?;
+
+                Ok(())
+            },
+            Self::RawCertsBytes(raw_certs_bytes) => {
+                // u8
+                1u8.append_to_bytes(bytes)?;
+
+                // Vec<Vec<u8>>
+                raw_certs_bytes.append_to_bytes(bytes)?;
+
+                Ok(())
+            },
+        }
+    }
+    fn extract_from_bytes(bytes: &Vec<u8>, index: &mut usize) -> Result<Self, Box<dyn Error>> where Self: Sized {
+        let enum_variant_byte = u8::extract_from_bytes(bytes, index)?;
+
+        match enum_variant_byte {
+            0 => {
+                let file_path = PathBuf::extract_from_bytes(bytes, index)?;
+
+                let server_public_key_file_bytes = Vec::<u8>::extract_from_bytes(bytes, index)?;
+
+                // attempt to save the bytes to the same file path
+                {
+                    let mut file = File::create(&file_path)?;
+                    file.write_all(&server_public_key_file_bytes)?;
+                }
+
+                Ok(Self::FilePath(file_path))
+            },
+            1 => {
+                Ok(Self::RawCertsBytes(Vec::<Vec<u8>>::extract_from_bytes(bytes, index)?))
+            },
+            _ => {
+                Err(ServerClientByteConError::UnexpectedEnumVariantByte {
+                    enum_variant_byte,
+                    enum_variant_name: String::from(std::any::type_name::<Self>()),
+                }.into())
+            }
+        }
+    }
+}
+
+pub struct ByteConPublicKey {
+    certificate: ByteConCertificate,
+}
+
+impl ByteConPublicKey {
+    pub fn new(certificate: ByteConCertificate) -> Self {
+        Self {
+            certificate,
+        }
+    }
+    pub fn get_certificates(&self) -> Result<Vec<Certificate>, Box<dyn Error>> {
+        match &self.certificate {
+            ByteConCertificate::FilePath(file_path) => {
+                let cert_file = std::fs::File::open(file_path)?;
+                let mut reader = std::io::BufReader::new(cert_file);
+                let certs = rustls_pemfile::certs(&mut reader)?
+                    .into_iter()
+                    .map(Certificate)
+                    .collect();
+                Ok(certs)
+            },
+            ByteConCertificate::RawCertsBytes(bytes) => {
+                let certs = bytes
+                    .into_iter()
+                    .map(|item| {
+                        Certificate(item.clone())
+                    })
+                    .collect();
+                Ok(certs)
+            },
+        }
+    }
+}
+
+impl ByteConverter for ByteConPublicKey {
+    fn append_to_bytes(&self, bytes: &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
+        self.certificate.append_to_bytes(bytes)?;
+        Ok(())
+    }
+    fn extract_from_bytes(bytes: &Vec<u8>, index: &mut usize) -> Result<Self, Box<dyn Error>> where Self: Sized {
+        Ok(Self {
+            certificate: ByteConCertificate::extract_from_bytes(bytes, index)?,
+        })
+    }
+}
+
+pub struct ByteConPrivateKey {
+    certificate: ByteConCertificate,
+}
+
+impl ByteConPrivateKey {
+    pub fn new(certificate: ByteConCertificate) -> Self {
+        Self {
+            certificate,
+        }
+    }
+    pub fn get_certificate(&self) -> Result<PrivateKey, Box<dyn Error>> {
+        match &self.certificate {
+            ByteConCertificate::FilePath(file_path) => {
+                let key_file = std::fs::File::open(file_path)?;
+                let mut reader = std::io::BufReader::new(key_file);
+                let keys = rustls_pemfile::pkcs8_private_keys(&mut reader)?;
+                Ok(PrivateKey(keys[0].clone()))
+            },
+            ByteConCertificate::RawCertsBytes(bytes) => {
+                Ok(PrivateKey(bytes[0].clone()))
+            },
+        }
+    }
+}
+
+impl ByteConverter for ByteConPrivateKey {
+    fn append_to_bytes(&self, bytes: &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
+        self.certificate.append_to_bytes(bytes)?;
+        Ok(())
+    }
+    fn extract_from_bytes(bytes: &Vec<u8>, index: &mut usize) -> Result<Self, Box<dyn Error>> where Self: Sized {
+        Ok(Self {
+            certificate: ByteConCertificate::extract_from_bytes(bytes, index)?,
+        })
     }
 }
 
@@ -186,8 +369,8 @@ where
 {
     bind_address: String,
     bind_port: u16,
-    public_key_file_path: PathBuf,
-    private_key_file_path: PathBuf,
+    public_key: ByteConPublicKey,
+    private_key: ByteConPrivateKey,
     message_processor: Arc<TMessageProcessor>,
 }
 
@@ -200,41 +383,24 @@ where
     pub fn new(
         bind_address: String,
         bind_port: u16,
-        public_key_file_path: PathBuf,
-        private_key_file_path: PathBuf,
+        public_key: ByteConPublicKey,
+        private_key: ByteConPrivateKey,
         message_processor: Arc<TMessageProcessor>
     ) -> Self {
         Self {
             bind_address,
             bind_port,
-            public_key_file_path,
-            private_key_file_path,
+            public_key,
+            private_key,
             message_processor,
         }
     }
     pub async fn start(&self) -> Result<(), Box<dyn Error>> {
-        let certs = {
-            let cert_file = std::fs::File::open(&self.public_key_file_path)?;
-            let mut reader = std::io::BufReader::new(cert_file);
-            let certs = rustls_pemfile::certs(&mut reader)?
-                .into_iter()
-                .map(Certificate)
-                .collect();
-            certs
-        };
-
-        let key = {
-            let key_file = std::fs::File::open(&self.private_key_file_path)?;
-            let mut reader = std::io::BufReader::new(key_file);
-            let keys = rustls_pemfile::pkcs8_private_keys(&mut reader)?;
-            PrivateKey(keys[0].clone())
-        };
-
         // configure the TLS server
         let tls_config = ServerConfig::builder()
             .with_safe_defaults()
             .with_no_client_auth()
-            .with_single_cert(certs, key)?;
+            .with_single_cert(self.public_key.get_certificates()?, self.private_key.get_certificate()?)?;
         let tls_acceptor = TlsAcceptor::from(Arc::new(tls_config));
 
         let listening_address = format!("{}:{}", self.bind_address, self.bind_port);
@@ -311,5 +477,10 @@ enum ServerClientByteConError {
     UnexpectedServerResponse {
         server_response: String,
         server_request: String,
+    },
+    #[error("Unexpected server public key file size {actual_file_size} after checking for last position {last_position}.")]
+    UnexpectedServerPublicKeyFileSize {
+        actual_file_size: usize,
+        last_position: usize,
     },
 }
